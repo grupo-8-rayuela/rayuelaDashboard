7468d459223089ebca6a45ab633bdfe6ff1ab129
Romina Rodes <104787916+RominaRodes@users.noreply.github.com>
2022-10-15 17:56:11 -0300

    prueba para pasar prps con estados y compartir info

--- src/components/ContentRowTop.js
+++ src/components/ContentRowTop.js
@@ -2,8 +2,66 @@ import React from 'react';
 import ContentRowCenter from './ContentRowCenter';
 import ContentRowDataCards from './ContentRowDataCards';
 import Chart from './Chart';
+import axios from 'axios';
+import {useEffect, useState} from 'react';
 
 function ContentRowTop(){
+	const [juguetes, setJuguetes] = useState([]);
+    const [users, setUsers] = useState([]);
+    const [orders, setOrders] = useState([]);
+
+    const products =  async () => {
+        const json = await axios("http://localhost:3001/api/products");//pego a mi api
+            {/* console.log(json) */}
+        setJuguetes(json.data.products);};
+    
+    const usuarios =  async () => {
+        const json = await axios("http://localhost:3001/api/users");//pego a mi api
+        
+        setUsers(json.data.users);}
+
+    const ventas = async () => {
+              const json = await axios("http://localhost:3001/api/orders");//pego a mi api
+                setOrders(json.data);
+                console.log(json.data)
+        }
+
+    useEffect(() => {
+            products();
+            usuarios();
+            ventas();
+            
+        }, [setJuguetes, setUsers, setOrders]);
+		let salesInDB = {
+			title: 'Total de Ventas',
+			color: 'primary', 
+			cuantity: `${orders.totalSales}$`,
+			icon: 'fa-clipboard-list'
+		}
+		
+		/* <!-- Total awards --> */
+		
+		let productsInDB = {
+			title:'Productos en Data Base', 
+			color:'success', 
+			cuantity: `${juguetes.length}`,
+			icon:'fa-award'
+		}
+		
+		/* <!-- Actors quantity --> */
+		
+		let usersInDB = {
+			title:'Usuarios en Data Base' ,
+			color:'warning',
+			cuantity:`${users.length}`,
+			icon:'fa-user-check'
+		}
+		
+	
+    
+
+
+
     return(
         <React.Fragment>
 				{/*<!-- Content Row Top -->*/}
@@ -13,7 +71,9 @@ function ContentRowTop(){
 					</div>
 				
 					{/*<!-- Content Row Movies-->*/}
-					<ContentRowDataCards />
+					<ContentRowDataCards
+					datacardprops = {[salesInDB, productsInDB, usersInDB]}
+					 />
 					<ContentRowCenter />
 					<Chart />
 	
